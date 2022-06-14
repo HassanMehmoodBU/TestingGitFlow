77769872214efd6d9dcf5b4225d131842bbb7c71
Muhammad Hassan <hassanmehmood.cs@gmail.com>
2022-06-14 13:43:11 +0200

    Feature 01 completed - 14062022.01

---
+++ TestApp/ConsoleApp1/Feature01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Feature01
+    {
+
+        public int feature_id { get; set; }
+        public string feature_name { get; set; }
+
+        public int feature_type { get; set; }
+
+        public Feature01(int id, string name, int type)
+        {
+            this.feature_id = id;
+            this.feature_name = name;
+            this.feature_type = type;
+        }
+
+
+    }
+}
