3e864ac9a0c8f743a888b13f1efee8ddf05d3333
HassanMehmoodBU <hassanmehmood.cs@gmail.com>
2022-06-22 10:07:23 +0200

    Update Program.cs from Git
    Update Program.cs from Git

--- TestApp/ConsoleApp1/Program.cs
+++ TestApp/ConsoleApp1/Program.cs
@@ -12,6 +12,8 @@ ConsoleApp1.Feature01 f1 = new ConsoleApp1.Feature01(1,"test01",1);
 
 Console.WriteLine(f1.ToString());
 
+
+Console.WriteLine("New Line added from GIT");
 Console.ReadKey();
     
 
