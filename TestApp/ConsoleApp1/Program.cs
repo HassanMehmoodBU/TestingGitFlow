ec978f182ae5bce2f5bae016336b559bd9b48248
Muhammad Hassan <hassanmehmood.cs@gmail.com>
2022-06-22 10:05:53 +0200

    Revert "Testing Stash 01"
    This reverts commit 243bda6eeada28f7bbe76ba4889c744d9cd7d4d7.

--- TestApp/ConsoleApp1/Program.cs
+++ TestApp/ConsoleApp1/Program.cs
@@ -11,8 +11,6 @@ ConsoleApp1.Feature01 f1 = new ConsoleApp1.Feature01(1,"test01",1);
 
 
 Console.WriteLine(f1.ToString());
-Console.WriteLine(f1.ToString());
-Console.WriteLine(f1.ToString());
 
 Console.ReadKey();
     
